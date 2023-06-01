a079ed9ae6c14125cd1511d578ebf747b533e776
Tejas <77725958+tejas-k3@users.noreply.github.com>
2023-06-01 16:48:18 +0530

    Python List based questions

---
+++ ListQues.py
@@ -0,0 +1,8 @@
+"""
+Create a list having elements 1,2,3,4,5.
+Write function for following opeartions :
+1. Swap any 2 elements of which indexes are passed eg 0, 4 --> 5,2,3,4,1
+2. Maximum value of that list ---> 5
+3. Minimum value of that list --> 2
+4. Check if list is sorted in ascending order. --> True
+"""
